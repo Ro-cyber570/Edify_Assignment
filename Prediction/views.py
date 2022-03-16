45f5db92d4863c74a4fc274b4c6f7dd6eeb942da
Ro-cyber570 <rohitchitale1@gmail.com>
2022-03-16 22:18:32 +0530

    Second Commit

--- Prediction/views.py
+++ Prediction/views.py
@@ -1,3 +1,4 @@
+from http.client import HTTPResponse
 from pickle import FALSE
 from django.shortcuts import render
 import csv,io
@@ -10,6 +11,7 @@ from sklearn.model_selection import train_test_split
 from sklearn import metrics
 from .models import Diabetes
 import plotly.express as px
+import json
 
 def index(request):
     df=pd.read_csv(r'C:\Users\G15-D560542WIN9W\Desktop\projects\DS_Project\Prediction\diabetes.csv')
@@ -27,18 +29,20 @@ def index(request):
 def csv_upload(request):
 	template="index.html"
 	if request.method=="GET":
-		return render(request,template)
+		return render(request,template)	
 	csv_file=request.FILES['file']
 	if not csv_file.name.endswith('.csv'):
 		messages.error(request,"Not a csv file")
 	df=pd.read_csv(csv_file)
 	target=request.POST.get("target",False)
+	n_neighbours=request.POST.get("n_neighbours",False)
+	weights=request.POST.get("weights",False)
 	s=False
 	for column in df.columns:
 		if column==target:
 			s=True
-	list1=[]
 	if s==True:
+		list1=[]
 		for coloumn in df.columns:
 			if coloumn==target:
 				y=df[target]
@@ -46,27 +50,33 @@ def csv_upload(request):
 				list1.append(coloumn)
 		x=df[list1]
 		X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state=42)
-		n_neighbours=request.POST.get("n_neighbours",False)
-		weights=request.POST.get("weights",False)
 		knn = KNeighborsClassifier(n_neighbors=int(n_neighbours),weights=weights)
 		knn.fit(X_train, y_train)
 		y_pred=knn.predict(X_test)
-		context={"accuracy":metrics.accuracy_score(y_test,y_pred)}
-		y_score = knn.predict_proba(X_test)[:,1]
-
-		fig = px.scatter(
-   		X_test, x= 'Glucose', y="Insulin",
-    	color=y_score, color_continuous_scale='RdBu',
-    	symbol=y_test, symbol_map={'0': 'square-dot', '1': 'circle-dot'},
-    	labels={'symbol': 'label', 'color': 'score of <br>first class'}
-		)
-		fig.update_traces(marker_size=12, marker_line_width=1.5)
-		fig.update_layout(legend_orientation='h')
-		context['graph']=fig.to_html()
+		context={"accuracy":metrics.accuracy_score(y_test,y_pred),"usedcols":list1}
+		list2=request.POST.getlist("checks[]")
+		if len(list2)>1:
+			x=df[list2]
+			X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state=42)
+			knn = KNeighborsClassifier(n_neighbors=int(n_neighbours),weights=weights)
+			knn.fit(X_train, y_train)
+			y_pred=knn.predict(X_test)
+			context={"accuracy":metrics.accuracy_score(y_test,y_pred),"usedcols":list2}
+			return render(request,template,context)
 		return render(request,template,context)
-	else:
-		context={"error":"Target value not valid"}
-		return (request,template,context)
+		
+			
+	#else:
+	#	list1=request.POST.getlist("checks[]")
+	#	csv_file=request.FILES['file']
+	#	df=pd.read_csv(csv_file)
+	#	x=df[list1]
+	#	X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state=42)
+	#	knn = KNeighborsClassifier(n_neighbors=int(n_neighbours),weights=weights)
+	##	y_pred=knn.predict(X_test)
+	#	context={"accuracy":metrics.accuracy_score(y_test,y_pred),"usedcols":list1}
+	#	return render(request,template,context)
+
 
 
 	
